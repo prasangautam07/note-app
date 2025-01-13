f039f891935362a675e786afa180a184a341b421
prasangautam07 <gprasan729@gmail.com>
2025-01-13 23:36:47 +0545

    updated

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,7 +80,8 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:5173",
 "http://localhost:5173",
 "https://note-iqtd7htze-prasan-gautams-projects.vercel.app",
-"https://note-app-beta-beryl.vercel.app"
+"https://note-app-beta-beryl.vercel.app",
+"https://note-app-2nq4.onrender.com"
 ]
 
 # Database
